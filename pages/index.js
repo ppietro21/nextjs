c173ff95eba2d7236d57d47a10de3d06769418cb
ppietro21 <85501097+ppietro21@users.noreply.github.com>
2021-06-07 16:18:11 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,13 +5,13 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Los mejores links de páginas</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Test h1 with link Theteabox.es <a href="https://theteabox.es">Link</a>
+          Links de los mejores links de páginas
         </h1>
 
         <p className={styles.description}>
@@ -64,23 +64,6 @@ Si quieres conocer las distintas opciones que hay en la moda de trajes para homb
             <p>Infusiones para adelgazar, energia, relajantes y mucho más</p>
           </a>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
         </div>
       </main>
 
